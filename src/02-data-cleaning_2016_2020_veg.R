6e581721865ec7eb052596117b5b7fb7fa74faa1
XIE <garlandxie@gmail.com>
2021-04-29 12:51:25 -0400

    Added sanity checks!

--- src/02-data-cleaning_2016_2020_veg.R
+++ src/02-data-cleaning_2016_2020_veg.R
@@ -3,6 +3,7 @@ library(here)
 library(dplyr)
 library(readr)
 library(visdat)
+library(validate)
 
 # import -----------------------------------------------------------------------
 
@@ -37,4 +38,53 @@ tail(mw_tidy, n = 5)
 vis_dat(mw_tidy)
 vis_miss(mw_tidy)
 
-# 
+# data cleaning ----------------------------------------------------------------
+
+mw_tidy2 <- mw_tidy %>%
+  mutate(section = case_when(
+    section == "4-1" ~ "4.1",
+    section == "4-2" ~ "4.2", 
+    section == "4-3" ~ "4.3",
+    section == "4-4" ~ "4.4",
+    section == "7-1" ~ "7.1",
+    TRUE ~ section)
+  ) %>%
+  mutate(season = case_when(
+    season == "summer" ~ "Summer",
+    TRUE ~ season)
+    )
+
+# validate ---------------------------------------------------------------------
+
+rules_16_20 <- validator(
+  five_quads = quadrat %in% c(1, 2, 3, 4, 5),
+  summer     = season == "Summer",
+  sol_TF     = solitary %in% c(TRUE, FALSE),
+  cf_TF      = cf %in% c(TRUE, FALSE),
+  pos_cover  = cover >= 0, 
+  year_16_20 = year %in% c(2016, 2018, 2019, 2020),
+  sites      = site %in% 
+    c("A", "B", "C", "D", "E", "F",
+      "G", "H", "I", "J", "K", "L", 
+      "M", "N", "O", "P", "R", "V", 
+      "W", "X"),
+  sections   = section %in% 
+    c("1.1", "1.2", "1.3", "1.4",
+      "3.2", "3.3", 
+      "4.1", "4.2", "4.3",  "4.4", 
+      "5.1", "5.3", "5.4",
+      "6.1", "6.2", "6.4",
+      "7.1")
+)
+
+mw_tidy2_out <- confront(mw_tidy2, rules_16_20)
+summary(mw_tidy2_out)
+
+# save to disk -----------------------------------------------------------------
+
+write.csv(
+  x = mw_tidy2,
+  file = here("data/final", "veg_surveys_2016-2020.csv"),
+  row.names = FALSE
+)
+
